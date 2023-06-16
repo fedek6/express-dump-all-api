752ba600139d2c9387b975dddc6e972b4047e486
Konrad Fedorczyk <fedek6@gmail.com>
2023-06-16 13:41:23 +0200

    Add better logging

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,9 +1,17 @@
 import * as fs from "fs";
+import * as path from "path";
 
-export function format(log: any) {
-  return JSON.stringify(log, null, 2);
-}
+/**
+ * Formats an object to string
+ * @param log
+ * @returns
+ */
+export const format = (log: any) => JSON.stringify(log, null, 2);
 
+/**
+ * Creates a counter function using a closure
+ * @returns
+ */
 export function createCounter() {
   let count = 0;
 
@@ -14,11 +22,43 @@ export function createCounter() {
   return counter;
 }
 
-export const fileLog = (text: string) =>
-  fs.writeFileSync(createLogFileName(), `Time: ${Date.now()}\n${text}` + "\n", {
+/**
+ * Logs text to a file
+ * @param text
+ * @param dir
+ */
+export const fileLog = (text: string, dir = "./") => {
+  const logPath = path.join(dir, createLogFileName());
+
+  fs.writeFileSync(logPath, `${text}` + "\n", {
     flag: "a",
   });
+};
 
+/**
+ * Returns the current date and time in the format: YYYY-MM-DD HH:MM:SS
+ * @returns
+ */
+export function getCurrentDateTime() {
+  const date = new Date();
+
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  };
+
+  return date.toLocaleString("en-US", options);
+}
+
+/**
+ * Creates a log file name in the format: log_YYYY-MM-DD.log
+ * @returns
+ */
 function createLogFileName(): string {
   const currentDate = new Date();
   const year = currentDate.getFullYear();
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ import * as dotenv from "dotenv";
 import formbody from "@fastify/formbody";
 import multipart from "@fastify/multipart";
 import { color } from "console-log-colors";
-import { format, createCounter, fileLog } from "./helpers";
+import { format, createCounter, fileLog, getCurrentDateTime } from "./helpers";
+import * as path from "path";
+import { cwd } from "process";
 
 dotenv.config();
+
+const LOG_PATH = path.join(cwd(), process.env.LOG_PATH ?? "./logs");
 
 const fastify = Fastify();
 fastify.register(formbody);
@@ -16,6 +20,12 @@ const counter = createCounter();
 fastify.all("*", async (request, reply) => {
   reply.type("application/json").code(200);
   const count = counter();
+  const { remoteAddress } = request.socket;
+
+  fileLog(
+    `---\n[${getCurrentDateTime()}]\nIncoming data ${count} from ${remoteAddress}`,
+    LOG_PATH
+  );
 
   console.log(color.bold.bgGreen(`👃 Incoming request: ${count}`));
   console.log(color.red.underline("Headers"));
@@ -23,12 +33,12 @@ fastify.all("*", async (request, reply) => {
   console.log(color.red.underline("Body"));
   console.log(color.green(format(request.body)));
 
-  fileLog(format(request.body));
+  fileLog(`Body:\n${format(request.body)}`, LOG_PATH);
 
   console.log(color.red.underline("Query params"));
   console.log(color.green(format(request.query)));
 
-  fileLog(format(request.query));
+  fileLog(`Query:\n${format(request.query)}`, LOG_PATH);
 
   console.log(color.red.underline("Info"));
   console.log(
@@ -47,6 +57,5 @@ fastify.all("*", async (request, reply) => {
 
 fastify.listen({ port: +process.env.PORT! }, (err, address) => {
   if (err) throw err;
-  console.log(`🙉 Server is listening on ${process.env.PORT}`);
+  console.log(`🙉 Server is listening on ${address}`);
 });
-``;
